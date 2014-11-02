da0da25c9ecc70a7e506d7eb2073316f2db0754c
Anna Jõgi a.k.a Libahunt <annaj6gi@gmail.com>
2014-11-03 01:31:40 +0200

    Added code comments

--- yarnmachine.ino
+++ yarnmachine.ino
@@ -16,15 +16,30 @@ D9, D10, D11 = PB1, PB2, PB3
 Thread distributer motor D3 = PD3
 */
 
-unsigned long threadLostTime = 3000;
+/*If problem with thrread is detected then respective motor speeds up and after 
+threadLostTime passes and the problem has not gone away then the machine will stop*/
+unsigned long threadLostTime = 3000;//time in MILLISECONDS
 
-/*system speed*/
-int speedState = 0;
-int prevSpeedState = 1;
-/*each motor own speed coefficent*/
+/*System speed is set with SpeedUp and SpeedDown buttons
+If machine is stopped (by buttonpress or thread breakage detection) then 
+"start" buttonspress resumes the previous speed.
+While machine is stopped, the SpeedDown button works, but SpeedUp does not.*/
+int speedState = 0; //System speed has value from 0 to 10
+int prevSpeedState = 1; //Stores previous speed for resuming
+
+/*Button states needed for detecting beginning of a push*/
+boolean btSpeedUpState;
+boolean btSpeedDownState;
+boolean btStartStopState;
+boolean btSpeedUpLastState;
+boolean btSpeedDownLastState;
+boolean btStartStopLastState;
+
+/*Each motor has it's own speed coefficent that is 1 usually but goes to 1.25
+when thread problem is detected*/
 float motorSpeedCoefs[5] = {1, 1, 1, 1, 1};
 
-/*tension sensors functionality*/
+/*tension sensors functionality: states and problem start time*/
 boolean tensionProblem[5] = {0,0,0,0,0};
 boolean tensionProblemLast[5] = {0,0,0,0,0};
 unsigned long tensionDebounceStart[5] = {0,0,0,0,0};
@@ -32,12 +47,7 @@ unsigned long tensionDebounceStart[5] = {0,0,0,0,0};
 /*helpers*/
 byte portMask = 0x01;
 int i, j, k=0;
-boolean btSpeedUpState;
-boolean btSpeedDownState;
-boolean btStartStopState;
-boolean btSpeedUpLastState;
-boolean btSpeedDownLastState;
-boolean btStartStopLastState;
+
 
 /*input buttons as debounced library objecs*/
 DebouncedButton btSpeedUp = DebouncedButton(8,50);
@@ -50,7 +60,6 @@ void setup() {
   /*debug*/#ifdef DEBUG
     /*debug*/Serial.begin(9600);
   /*debug*/#endif
-  DPL("setup");
   
   /*Pin modes*/
   DDRC = DDRC & ~0x1F;//tension sensors inputs
@@ -58,39 +67,35 @@ void setup() {
   DDRB = DDRB | 0xE;//motor outputs
   //button inputs are handeled in library objects
   
-  digitalWrite(3, 0);
-  digitalWrite(5, 0);
-  digitalWrite(6, 0);
-  digitalWrite(9, 0);
-  digitalWrite(10, 0);
-  digitalWrite(11, 0);
-  
-  DPL("outputs low");
+
   
 }
 
 void loop() {
   
-/*Input buttons functionality*/
+/*** Input buttons functionality ***/
+
+  //read current button states
   btStartStopState = btStartStop.dbRead();
   btSpeedUpState = btSpeedUp.dbRead();
   btSpeedDownState = btSpeedDown.dbRead();
   
+  //detect falling edges only
   if(!btStartStopState && btStartStopLastState) {
-    if (speedState>0) {
+    if (speedState >0 ) {//machine is running - stop the machine
       prevSpeedState = speedState;
       speedState = 0;
     }
     else {
-      speedState = prevSpeedState;
+     speedState = prevSpeedState;//machine not running - turn on the machine, resume last speed 
     }
   }
   else if(!btSpeedDownState && btSpeedDownLastState) {
-    if (speedState > 1) {
+    if (speedState > 1) {//if machine is running reduce speed
       speedState--;
       prevSpeedState = speedState;
     }
-    else {
+    else {//if machine is stopped reduce the speed that will be resumed later
       prevSpeedState--;
     }
   }
@@ -100,55 +105,65 @@ void loop() {
       prevSpeedState = speedState;
     }
   }
-  if (prevSpeedState == 0) prevSpeedState = 1;
+  if (prevSpeedState == 0) prevSpeedState = 1;//resumable speed has to be higher than 0
   
+  //save button states for next loop
   btStartStopLastState = btStartStopState;
   btSpeedUpLastState = btSpeedUpState;
   btSpeedDownLastState = btSpeedDownState;
 
   
-/*Tension sensors functionality*/
-  portMask = 0x01;
+  
+/*** Tension sensors functionality ***/
+
+  portMask = 0x01;//for scanning over port C 0-4
   for (i=0; i<5; i++) {
-    tensionProblemLast[i] = tensionProblem[i];
     
-    if ((PINC & portMask) == 0) {
+    if ((PINC & portMask) == 0) {//has tension problem
+    
       tensionProblem[i] = 1;
-      motorSpeedCoefs[i] = 1.25;
-      if (tensionProblem[i] && !tensionProblemLast[i]) {
+      motorSpeedCoefs[i] = 1.25;//increase speed coefficent to balance possible loose thread
+      if (tensionProblem[i] && !tensionProblemLast[i]) {//if tension problem just started, save the time
         tensionDebounceStart[i] = millis();
       }
-    }
-    else {
-      tensionProblem[i] = 0;
-      motorSpeedCoefs[i] = 1;
-    }
-    
-    if (tensionProblem[i] == 1) {
-      if (millis() - tensionDebounceStart[i] > threadLostTime) {
-        prevSpeedState = speedState;
-        speedState = 0;
+      
+      if (millis() - tensionDebounceStart[i] > threadLostTime) {//if tension problem has been on for long
+        prevSpeedState = speedState;//save speed state for resuming later
+        speedState = 0;//stop the machine
         for (j=0; j<5; j++) {
-          tensionProblem[j] = 0;
+          tensionProblem[j] = 0;//clear problem states
         }
-        DPL("tension problem lasted long");
       }
+      
     }
     
-    portMask = portMask << 1;
+    else {//all good
+      tensionProblem[i] = 0;
+      motorSpeedCoefs[i] = 1;//regular speed
+    }
+    
+    tensionProblemLast[i] = tensionProblem[i];//save tension problem state
+    portMask = portMask << 1;//move to next sensor
+    
   }
   
   
-/*Write pwm according to each motors own speed coefficent*/
+/*** Write pwm according to each motors own speed coefficent ***/
+
   analogWrite(11, 20*speedState*motorSpeedCoefs[0]);
   analogWrite(10, 20*speedState*motorSpeedCoefs[1]);
   analogWrite(9, 20*speedState*motorSpeedCoefs[2]);
   analogWrite(6, 20*speedState*motorSpeedCoefs[3]);
   analogWrite(5, 20*speedState*motorSpeedCoefs[4]);
   
-  /*turn thread distributor on or off also*/
+/** turn thread distributor on or off also ***/
   analogWrite(3, speedState*20);
   
+  
+/*** Print some debugging numbers to serial ***/ 
+
+#ifdef DEBUG
+
   if (k=10) {
     DP("System speed: ");
     DP(speedState);
@@ -166,8 +181,7 @@ void loop() {
   }
   k++;
   
-  
-  
+#endif
   
   
   
